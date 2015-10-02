bc547a923b3f32f843e635c4b88e40ba02679428
Kevin Vermeulen <kevin.vermeule@telecom-bretagne.eu>
2015-10-02 13:23:12 +0200

    sampleCOntroller

---
+++ ns-webApp/src/main/java/ns/controllers/SampleController.java
@@ -0,0 +1,20 @@
+package ns.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/rest")
+public class SampleController {
+	
+	
+	@RequestMapping(value="/getHello", method = RequestMethod.GET)
+	@ResponseBody
+	public String getTest(){
+		System.out.println("Rest Successful");		
+		return "Rest Request Test";
+	}
+	
+}
